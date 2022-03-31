8e2bd0919156face9d87313e77291f34db196008
Kyaw-Zin-Latt <kyawzinlat43021@gmail.com>
2022-04-01 00:46:32 +0630

    up

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    fontFamily:{
+      body: ['Montserrat']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
